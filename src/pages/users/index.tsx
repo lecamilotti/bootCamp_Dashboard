464d81e62ffe6d85e3c79e189b334ce170332288
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-21 13:20:32 +0100

    pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Text, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function  UserList() {
@@ -60,8 +61,8 @@ export default function  UserList() {
                 </Td>
               </Tr>
             </Tbody>
-
           </Table>
+          <Pagination />
 
         </Box>
       </Flex>
