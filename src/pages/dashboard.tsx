102df3a9266bc01de926a662ba14c3caadba39b1
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-21 08:39:55 +0100

    chart

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,57 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const options ={
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    theme: "dark"
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2021-03-18T00:00:00.000Z",
+      "2021-03-19T00:00:00.000Z",
+      "2021-03-20T00:00:00.000Z",
+      "2021-03-21T00:00:00.000Z",
+      "2021-03-22T00:00:00.000Z",
+      "2021-03-23T00:00:00.000Z",
+      "2021-03-24T00:00:00.000Z",
+      "2021-03-25T00:00:00.000Z",
+    ]
+
+    
+  }
+
+};
+
+const series = [
+  { name: 'New Subscribes', data: [30, 200, 45, 50, 212, 123, 70, 54] },
+]
 
 export default function Dashboard() {
   return (
@@ -14,18 +65,20 @@ export default function Dashboard() {
           p="8"
           bg="gray.800"
           borderRadius={8}
-          //pb="4"
+          pb="4"
           >
             <Text fontSize="lg" mb="4">Weekly new subscribes</Text>
+            <Chart options={options} series={series} type="area" height={160} />
 
           </Box>
           <Box
           p="8"
           bg="gray.800"
           borderRadius={8}
-          //pb="4"
+          pb="4"
           >
             <Text fontSize="lg" mb="4">Opening Info</Text>
+            <Chart options={options} series={series} type="area" height={160} />
 
           </Box>
         </SimpleGrid>
