0b0b311737e7bc20ca97f29b11a8f3ac514677c8
Leandro Camilotti <lecamilotti@gmail.com>
2022-01-15 11:18:41 +0100

    shift fetch for axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,19 +15,18 @@ import {
   useBreakpointValue,
   Spinner,
 } from "@chakra-ui/react";
-import { date } from "faker/locale/zh_TW";
 import Link from "next/link";
-import { RiAddLine, RiEmpathizeLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { useQuery } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+    const {data} = await api.get("users");
 
     const users = data.users.map((user) => {
       return {
