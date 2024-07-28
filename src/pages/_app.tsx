5f138f7686c37bc6783caf31fab546b35622fa0a
Leandro Camilotti <lecamilotti@gmail.com>
2024-07-28 11:49:42 +0200

    trying to update this dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
-import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { theme } from "../styles/theme";
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { theme } from '../styles/theme';
 
-import { QueryClient, QueryClientProvider } from "react-query";
-import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
-import { makeServer } from "../services/mirage";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,117 +1,139 @@
+import NextLink from 'next/link';
 import {
   Box,
-  Text,
   Button,
   Checkbox,
   Flex,
   Heading,
   Icon,
+  Link,
+  Spinner,
   Table,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
   Tr,
   useBreakpointValue,
-  Spinner,
-} from "@chakra-ui/react";
-import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+} from '@chakra-ui/react';
+import { RiAddLine } from 'react-icons/ri';
 
-import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
-import { Sidebar } from "../../components/Sidebar";
-import { useUsers } from "../../services/hooks/useUsers";
+import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
+import { Sidebar } from '../../components/Sidebar';
+import { getUsers, useUsers } from '../../services/hooks/useUsers';
+import { useState } from 'react';
+import { queryClient } from '../../services/queryClient';
+import { api } from '../../services/api';
+import { GetServerSideProps } from 'next';
 
-export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+export default function UserList({ users }) {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
-    md: true,
+    lg: true,
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(
+      ['user', userId],
+      async () => {
+        const response = await api.get(`users/${userId}`);
+
+        return response.data;
+      },
+      {
+        staleTime: 1000 * 60 * 10, // 10 minutes
+      }
+    );
+  }
 
   return (
     <Box>
       <Header />
-      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-          <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">
-              Users
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+          <Flex mb='8' justify='space-between' align='center'>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
               {!isLoading && isFetching && (
-                <Spinner size="sm" color="gray.500" ml="4" />
+                <Spinner size='sm' color='gray.500' ml='4' />
               )}
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href='/users/create' passHref>
               <Button
-                as="a"
-                size="sm"
-                fontSize="sm"
-                colorScheme="pink"
-                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                as='a'
+                size='sm'
+                fontSize='sm'
+                colorScheme='pink'
+                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
               >
-                Add New User
+                Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
+
           {isLoading ? (
-            <Flex justify="center">
+            <Flex justify='center'>
               <Spinner />
             </Flex>
           ) : error ? (
-            <Flex justify="center">
-              <Text>Error to get user data</Text>
+            <Flex justify='center'>
+              <Text>Falha ao obter dados dos usuários.</Text>
             </Flex>
           ) : (
             <>
-              <Table colorScheme="whiteAlpha">
+              <Table colorScheme='whiteAlpha'>
                 <Thead>
                   <Tr>
-                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                      <Checkbox colorScheme="pink" />
+                    <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                      <Checkbox colorScheme='pink' />
                     </Th>
-                    <Th>User</Th>
-                    {isWideVersion && <Th>Date</Th>}
-
-                    <Th width="8"></Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
-                        <Td px={["4", "4", "6"]}>
-                          <Checkbox colorScheme="pink" />
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme='pink' />
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
-                            <Text fontSize="sm" color="gray.300">
+                            <Link
+                              color='purple.400'
+                              onMouseEnter={() => handlePrefetchUser(user.id)}
+                            >
+                              <Text fontWeight='bold'>{user.name}</Text>
+                            </Link>
+                            <Text fontSize='sm' color='gray.300'>
                               {user.email}
                             </Text>
                           </Box>
                         </Td>
                         {isWideVersion && <Td>{user.createdAt}</Td>}
-                        <Td>
-                          <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="purple"
-                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                          >
-                            {isWideVersion ? "Edit" : ""}
-                          </Button>
-                        </Td>
                       </Tr>
                     );
                   })}
                 </Tbody>
               </Table>
-              <Pagination />
+
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
@@ -119,3 +141,13 @@ export default function UserList() {
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
